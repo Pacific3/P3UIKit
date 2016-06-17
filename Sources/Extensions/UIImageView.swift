c9923d7b8022faaaf3be6fde9e52c8fe59d0c9c1
Oscar Swanros <oscar@swanros.com>
2016-06-17 11:49:16 -0500

    Extension on UIimageView

---
+++ Sources/Extensions/UIImageView.swift
@@ -0,0 +1,14 @@
+//
+//  UIImageView.swift
+//  P3UIKit
+//
+//  Created by Oscar Swanros on 6/17/16.
+//  Copyright © 2016 Pacific3. All rights reserved.
+//
+
+extension UIImageView {
+    public func p_setImage<I: ImageConvertible>(image: I) {
+        self.image = image.image()
+    }
+}
+
