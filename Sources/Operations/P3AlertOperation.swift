08ab569f0cb5725048dafa1c8c717550988253c7
Oscar Swanros <oscar@swanros.com>
2016-06-17 12:01:31 -0500

    Add P3AlertOperation

--- Sources/Operations/P3AlertOperation.swift
+++ Sources/Operations/P3AlertOperation.swift
@@ -6,4 +6,67 @@
 //  Copyright © 2016 Pacific3. All rights reserved.
 //
 
-import Foundation
+
+public final class P3AlertOperation: P3Operation {
+    private let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+    private let presentationContext: UIViewController?
+    
+    public var title: String? {
+        get {
+            return alertController.title
+        }
+        set {
+            alertController.title = newValue
+            name = newValue
+        }
+    }
+    
+    public var message: String? {
+        get {
+            return alertController.message
+        }
+        set {
+            alertController.message = newValue
+        }
+    }
+    
+    public init(presentationContext: UIViewController? = nil) {
+        self.presentationContext = presentationContext ?? UIApplication.shared().keyWindow?.rootViewController
+        super.init()
+        addCondition(condition: AlertPresentation())
+        addCondition(condition: P3MutuallyExclusiveOperationCondition<UIViewController>())
+    }
+    
+    public func addAction(title: String, style: UIAlertActionStyle = .default, handler: (P3AlertOperation) -> Void = { _ in }) {
+        let action = UIAlertAction(title: title, style: style) { [weak self] _ in
+            if let strongSelf = self {
+                handler(strongSelf)
+            }
+            
+            self?.finish()
+        }
+        
+        alertController.addAction(action)
+    }
+    
+    
+    override public func execute() {
+        guard let presentationContext = presentationContext else {
+            finish()
+            
+            return
+        }
+        
+        p3_executeOnMainThread { [unowned self] in
+            if self.alertController.actions.isEmpty {
+                self.addAction(title: "Ok")
+            }
+            
+            presentationContext.present(self.alertController, animated: true, completion: nil)
+        }
+    }
+}
+
+public enum Alert { }
+public typealias AlertPresentation = P3MutuallyExclusiveOperationCondition<Alert>
+
