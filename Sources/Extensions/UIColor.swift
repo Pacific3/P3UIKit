4ad019c6e3185f0f72954bc0ee436abdb14c1a42
Oscar Swanros <oscar@swanros.com>
2017-03-16 13:11:14 -0600

    Squashed commit of the following:
    commit c8564703d07d76c1b82ffbf73c88b424b6f1eeb2
    Author: Oscar Swanros <oscar@swanros.com>
    Date:   Thu Mar 16 13:11:06 2017 -0600
    
        update to master deps
    
    commit bd3a0d8814e868e87dd1fd784fcc7af9136dccf0
    Author: Oscar Swanros <oscar@swanros.com>
    Date:   Thu Mar 16 12:46:04 2017 -0600
    
        add caching to uicolors
    
    commit 8a32f77ad29203d2546b5434b170f7102be425a7
    Author: Oscar Swanros <oscar@swanros.com>
    Date:   Thu Mar 16 12:45:32 2017 -0600
    
        update deps
    
    commit d532d2226f4aa6299c3b050910be994a6b3ebaad
    Author: Oscar Swanros <oscar@swanros.com>
    Date:   Mon Feb 13 22:40:54 2017 -0600
    
        Update to Swift 3.1

--- Sources/Extensions/UIColor.swift
+++ Sources/Extensions/UIColor.swift
@@ -7,51 +7,61 @@
 //
 
 public extension UIColor {
+    private static var _colorCache: [String:UIColor] = [:]
+    
     public class func p3_fromHexColorConvertible<C: HexColorConvertible>(hexColorConvertible: C) -> UIColor {
         return UIColor(p3_hex: hexColorConvertible.hexColor)
     }
     
     public convenience init(p3_hex hex: String) {
-        var red:   CGFloat = 0.0
-        var green: CGFloat = 0.0
-        var blue:  CGFloat = 0.0
-        var alpha: CGFloat = 1.0
-        
-        if hex.hasPrefix("#") {
-            let index   = hex.index(after: hex.startIndex)
-            let hex     = hex.substring(from: index)
-            let scanner = Scanner(string: hex)
-            var hexValue: CUnsignedLongLong = 0
-            if scanner.scanHexInt64(&hexValue) {
-                switch hex.characters.count {
-                case 3:
-                    red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
-                    green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
-                    blue  = CGFloat(hexValue & 0x00F)              / 15.0
-                case 4:
-                    red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
-                    green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
-                    blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
-                    alpha = CGFloat(hexValue & 0x000F)             / 15.0
-                case 6:
-                    red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
-                    green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
-                    blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
-                case 8:
-                    red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
-                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
-                    blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
-                    alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
-                default:
-                    print("Invalid HEX string, number of characters after '#' should be either 3, 4, 6 or 8")
+        if let c = UIColor._colorCache[hex] {
+            self.init(cgColor: c.cgColor)
+        } else {
+            var red:   CGFloat = 0.0
+            var green: CGFloat = 0.0
+            var blue:  CGFloat = 0.0
+            var alpha: CGFloat = 1.0
+            
+            if hex.hasPrefix("#") {
+                let index   = hex.index(after: hex.startIndex)
+                let hex     = hex.substring(from: index)
+                let scanner = Scanner(string: hex)
+                var hexValue: CUnsignedLongLong = 0
+                if scanner.scanHexInt64(&hexValue) {
+                    switch hex.characters.count {
+                    case 3:
+                        red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
+                        green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
+                        blue  = CGFloat(hexValue & 0x00F)              / 15.0
+                    case 4:
+                        red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
+                        green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
+                        blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
+                        alpha = CGFloat(hexValue & 0x000F)             / 15.0
+                    case 6:
+                        red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
+                        green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
+                        blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
+                    case 8:
+                        red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                        green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                        blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+                        alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+                    default:
+                        print("Invalid HEX string, number of characters after '#' should be either 3, 4, 6 or 8")
+                    }
+                } else {
+                    print("Scan HEX error")
                 }
             } else {
-                print("Scan HEX error")
+                print("Invalid HEX string, missing '#' as prefix")
             }
-        } else {
-            print("Invalid HEX string, missing '#' as prefix")
+            
+            let color = UIColor(red: red, green: green, blue: blue, alpha: alpha)
+            UIColor._colorCache[hex] = color
+            
+            self.init(red:red, green:green, blue:blue, alpha:alpha)
         }
-        self.init(red:red, green:green, blue:blue, alpha:alpha)
     }
 }
 
