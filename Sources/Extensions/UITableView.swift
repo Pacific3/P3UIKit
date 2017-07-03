b77e36b5db2f6f52b42af72d7d18508207d44460
Oscar Swanros <oscar@swanros.com>
2017-07-03 14:49:42 -0500

    Add extension to support autolayout on table view headers

--- Sources/Extensions/UITableView.swift
+++ Sources/Extensions/UITableView.swift
@@ -23,5 +23,35 @@
         private static func reuseIdentifier<T: UITableViewCell>(class _class: T.Type) -> String {
             return (Bundle(for: _class).bundleIdentifier ?? "_P3BundleIdentifier") + String(describing: _class)
         }
+        
+        func p3_layoutTableHeaderView() {
+            guard let headerView = self.tableHeaderView else { return }
+            headerView.translatesAutoresizingMaskIntoConstraints = false
+            
+            let headerWidth = headerView.bounds.size.width;
+            let temporaryWidthConstraints = NSLayoutConstraint.constraints(
+                withVisualFormat: "[headerView(width)]",
+                options: NSLayoutFormatOptions(rawValue: UInt(0)),
+                metrics: ["width": headerWidth],
+                views: ["headerView": headerView]
+            )
+            
+            headerView.addConstraints(temporaryWidthConstraints)
+            
+            headerView.setNeedsLayout()
+            headerView.layoutIfNeeded()
+            
+            let headerSize = headerView.systemLayoutSizeFitting(UILayoutFittingCompressedSize)
+            let height = headerSize.height
+            var frame = headerView.frame
+            
+            frame.size.height = height
+            headerView.frame = frame
+            
+            self.tableHeaderView = headerView
+            
+            headerView.removeConstraints(temporaryWidthConstraints)
+            headerView.translatesAutoresizingMaskIntoConstraints = true
+        }
     }
 #endif
